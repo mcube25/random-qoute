b95b5ef6142a7f7f0fe0afecc4a909eb2bed0b6d
mcube25 <mcube3567@gmail.com>
2020-02-06 15:31:49 +0100

    fun qoutes

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  $("#quote").on("click", function(){
+    $.ajax({
+      url: "https://api.icndb.com/jokes/random/1?firstName=borriss saint",
+      method : "GET",
+      success: function(result) {
+      var joke = result['value'][0]['joke'];
+      var author = result['value'][0]['categories'][0]
+          $('.quote-text').html(joke);
+          $('.cate').html(author);
+        }
+   });
+  });
+
+
+    $("button").addClass("animated shake");
+     $("section").addClass("animated shake");
+});
